e3183b7a8f8bd3abedd4ed35e20f10f5a6f432a8
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-09-22 06:04:26 +0200

    erster Pomodoro-Befehl: remainingTill

---
+++ pomodorogo/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		Anleitung()
+		return
+	}
+
+	command := strings.ToLower(os.Args[1])
+	if command == "remainingtill" {
+		RemainingTill()
+		return
+	}
+
+	fmt.Println("Unknown argument " + command)
+}
+
+func Anleitung() {
+	fmt.Println("PomodoroGo - Das Pomodoro-Tool (shoff, 2017-09-22)")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("   - RemainingTill xx (e.g. RemainingTill 18)")
+	fmt.Println("     Remaining Pomodoros that are possible until xx o'clock.")
+	fmt.Println("")
+}
+
+func RemainingTill() {
+	if len(os.Args) != 3 {
+		fmt.Println("Parameters not ok")
+		return
+	}
+
+	untilHour, err := strconv.ParseInt(os.Args[2], 10, 64)
+	if err != nil {
+		fmt.Println("Could not parse your hour parameter.")
+		return
+	}
+
+	var currentTime int
+	currentTime = time.Now().Hour()*60 + time.Now().Minute()
+	untilInMinutes := untilHour * 60
+	remaining := (int(untilInMinutes) - currentTime) / 30
+	fmt.Printf("There are %v pomodoros remaining till %v o'clock.\n", remaining, untilHour)
+}
